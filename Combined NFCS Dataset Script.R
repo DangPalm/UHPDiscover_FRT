96ce834b3149b1589f1118fcca26ad90e5bd1a5e
Dang Pham <dangp0337@gmail.com>
2025-06-24 12:39:31 -0400

    Update Combined NFCS Dataset Script.R
    Add a section to match combined NFCS dataset with COVID stringency indexes

--- Combined NFCS Dataset Script.R
+++ Combined NFCS Dataset Script.R
@@ -1,8 +1,10 @@
 library(tidyverse)
 
+#Read NFCS 2021 State by State and Investor Data
 state_2021 <- read_csv("NFCS state by state.csv")
 investor_2021 <- read_csv("NFCS 2021 Investor Data 221121.csv")
 
+#Read NFCS 2018 State by State and Investor Data
 state_2018 <- read_csv("NFCS 2018 State Data 190603.csv")
 investor_2018 <- read_csv("NFCS 2018 Investor Data 191107.csv")
 
@@ -36,12 +38,37 @@ combine_2018 <- combine_2018 %>%
     F2_6 = F2_6.y             # Used cash advance
   )
 
-#Bind rows of the 2 datasets; keep all columns & fill with NAs
-data <- bind_rows(combine_2018, combine_2021)
+#Bind rows of the 2 data sets; keep all columns & fill with NAs
+NFCS_data <- bind_rows(combine_2018, combine_2021)
 
 #Standardize for income question A8, change option 8, 9, 10 to 8
-data$A8_2021[data$A8_2021 %in% c(8,9,10)] <- 8
+NFCS_data$A8_2021[NFCS_data$A8_2021 %in% c(8,9,10)] <- 8
 
-write_csv(data,"dataset.csv")
-  
+#Export to data file
+write_csv(NFCS_data,"dataset.csv")
 
+#__________________________________________________________________
+#Match NFCS data with Covid stringency
+
+# Create a look up table to match state name with code
+state_lookup <- tibble(
+  NFCS_State_Code = 1:51,
+  State_Name = c(
+    "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado",
+    "Connecticut", "Delaware", "District of Columbia", "Florida", "Georgia", 
+    "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky",
+    "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota",
+    "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire",
+    "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota",
+    "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina",
+    "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington",
+    "West Virginia", "Wisconsin", "Wyoming"
+  )
+)
+# Join state_lookup with NFCS survey data
+NFCS_data <- NFCS_data %>% left_join(state_lookup, by=c("STATEQ" = "NFCS_State_Code"))
+# Join NFCS survey data with Covid stringency
+NFCS_data <- NFCS_data %>% left_join(covid_2021, by=c("State_Name" = "RegionName", "Year"="Year"))
+
+#Export to data file
+write_csv(NFCS_data,"NFCS_stringency_data.csv")
